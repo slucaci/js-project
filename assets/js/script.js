c11c5bfd8a687c1817e817004ef3bcd29b601f46
slucaci <serjlucaci@gmail.com>
2025-01-14 21:01:05 +0000

    Added function to display a question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,34 @@
-function runTest() {}
+const questions = [
+  {
+    question: "What does a red light mean?",
+    A: "STOP",
+    B: "GO",
+    C: "WAIT",
+    correct: ["A"],
+  },
+  {
+    question: "What does a green light mean?",
+    A: "STOP",
+    B: "GO",
+    C: "CAUTION",
+    correct: ["B"],
+  },
+];
 
-function questions() {}
+let currentQuestionIndex = 0;
 
-function showQuestion() {}
+function displayQuestion() {
+  const question = questions[currentQuestionIndex];
+  document.getElementById("question").innerText = question.question;
+  document.querySelector(".answer-btn[data-option='A']").innerText = question.A;
+  document.querySelector(".answer-btn[data-option='B']").innerText = question.B;
+  document.querySelector(".answer-btn[data-option='C']").innerText = question.C;
+}
 
-function timerStart() {}
+displayQuestion();
+
+// function timerStart() {}
+
+// function runTest() {}
+
+// function questions() {}
